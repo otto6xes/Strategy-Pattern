f4d0b7f38f6bb18344a8937727436f0fc9db5f67
otto6x <Ethan16js@gmail.com>
2026-03-23 12:13:27 -0700

    MeleeAttack.java and RangedAttack.java creation

---
+++ src/main/java/RangedAttack.java
@@ -0,0 +1,21 @@
+/**
+ * [Brief description of what this class does]
+ *
+ * @author Ethan Shelley
+ * @version 0.1.0
+ * @since 3/23/2026
+ */
+public class RangedAttack implements Attack{
+
+    Monster attacker;
+
+    public RangedAttack(Monster attacker){
+        this.attacker = attacker;
+    }
+
+    @Override
+    public int attack(Monster target){
+        System.out.println(attacker + " uses a ranged attack on " + target);
+        return -1;
+    }
+}
