7bb9ad5bfa58ef1658fdeb6c8351919fecc0f067
otto6x <Ethan16js@gmail.com>
2026-03-23 13:15:40 -0700

    Updated Monster.java, Imp.java, and Kobold.Java

--- src/main/java/Monsters/Kobold.java
+++ src/main/java/Monsters/Kobold.java
@@ -1,22 +1,33 @@
 package Monsters;
 
+import Abilities.RangedAttack;
+
 import java.util.HashMap;
 
 /**
  * [Brief description of what this class does]
  *
  * @author Ethan Shelley
- * @version 0.1.0
+ * @version 0.1.1
  * @since 3/23/2026
  */
 public class Kobold extends Monster{
 
     public Kobold(int maxHP, int xp, HashMap<String, Integer> items){
         super(maxHP, xp, items);
+        int maxStr = 15;
+        int maxDef = 6;
+        int maxAgi = 3;
+
+        attack = new RangedAttack(this);
+
+        strength = super.getAttribute(strength, maxStr);
+        defense = super.getAttribute(defense, maxDef);
+        agility = super.getAttribute(agility, maxAgi);
     }
 
     @Override
     public String toString(){
-        return "Kobold has : " + super.toString();
+        return "Monsters.Kobold has : " + super.toString();
     }
 }
--- src/main/java/Monsters/Monster.java
+++ src/main/java/Monsters/Monster.java
@@ -1,13 +1,16 @@
 package Monsters;
 
+import Abilities.Attack;
+
 import java.util.HashMap;
 import java.util.Objects;
+import java.util.Random;
 
 /**
  * [Brief description of what this class does]
  *
  * @author Ethan Shelley
- * @version 0.1.0
+ * @version 0.1.1
  * @since 3/23/2026
  */
 public abstract class Monster {
@@ -16,6 +19,11 @@ public abstract class Monster {
     private int maxHP;
     private int xp = 10;
     private HashMap<String, Integer> items;
+
+    int agility = 10;
+    int strength = 10;
+    int defense = 10;
+    Attack attack;
 
 
     public Monster(int maxHP, int xp, HashMap<String, Integer> items) {
@@ -47,6 +55,54 @@ public abstract class Monster {
 
     public int getMaxHP() {
         return maxHP;
+    }
+
+    public int getAgility() {
+        return agility;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    /*
+    this method returns an integer value between min and max
+    since rand.nextInt(n) give a number between 0 and n
+    we get a number between 0 and (max-min) then add min back in
+     */
+    public int getAttribute(int min, int max) {
+        Random rand = new Random();
+        if(min > max){
+            int temp = min;
+            min = max;
+            max = temp;
+        }
+        //returns a random number between min and max inclusive
+        return rand.nextInt(max - min) + min;
+    }
+
+    public boolean takeDamage(int damage){
+        System.out.println("The creature was hit for " + damage + " damage.");
+        if(damage > 0){
+            hp -= damage;
+        }
+        if(hp <= 0){
+            System.out.println("Oh no! The creature has perished!");
+            System.out.println(toString());
+            return false;
+        }
+        System.out.println(toString());
+        return true;
+    }
+
+    public int attackTarget(Monster target){
+        int damage = attack.attack(target);
+        target.takeDamage(damage);
+        return damage;
     }
 
     @Override
